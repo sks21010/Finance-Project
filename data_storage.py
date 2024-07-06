e35cd3ac941fe9411143fec295688864a22f388c
sks21010 <sri_kiran.sripada@uconn.edu>
2024-07-06 13:21:16 -0400

    Database storage for 95 stocks

--- data_storage.py
+++ data_storage.py
@@ -9,22 +9,26 @@ from psycopg2.extensions import register_adapter, AsIs
 psycopg2.extensions.register_adapter(np.int64, psycopg2._psycopg.AsIs)
 psycopg2.extensions.register_adapter(np.float64, psycopg2._psycopg.AsIs)
 
-excel_tickers_filepath = r"C:\Users\iamsk\sks_Python\Financial Data Storage Project\Yahoo Ticker Symbols - September 2017.xlsx"
-tickers_df = pd.read_excel(excel_tickers_filepath, header=3, usecols=[0,1,2,3,4])
-tickers = tickers_df['Ticker'].tolist() 
+# excel_tickers_filepath = r"C:\Users\iamsk\sks_Python\Financial Data Storage Project\Yahoo Ticker Symbols - September 2017.xlsx"
+# tickers_df = pd.read_excel(excel_tickers_filepath, header=3, usecols=[0,1,2,3,4])
+# tickers = tickers_df['Ticker'].tolist() 
 
-usa_tickers = set()
-for index, row in tickers_df.iterrows():
-    if row["Country"] == "USA":
-        usa_tickers.add(row["Ticker"])
+# usa_tickers = set()
+# for index, row in tickers_df.iterrows():
+#     if row["Country"] == "USA":
+#         usa_tickers.add(row["Ticker"])
+
+nasdaq_100_tickers_filepath = r"C:\Users\iamsk\sks_Python\Financial Data Storage Project\nasdaq_100_stocks.xlsx"
+nasdaq_100_tickers_df = pd.read_excel(nasdaq_100_tickers_filepath)
+nasdaq_100_tickers = nasdaq_100_tickers_df["Ticker"].tolist()
 
 
 # Database connection parameters
-dbname = 'stocks'
-user = 'postgres'
-password = 'Narik123'
-host = '127.0.0.1'
-port = '5432'
+dbname = "stocks"
+user = "postgres"
+password = "Narik123"
+host = "127.0.0.1"
+port = "5432"
 
 
 # Check if database exists
@@ -77,11 +81,22 @@ query = '''INSERT INTO prices (Date, Open, High, Low, Close, Adj_Close, Volume, 
             VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
             ON CONFLICT (Date, Ticker) DO NOTHING;'''
 
-for ticker in usa_tickers:
+# for ticker in usa_tickers:
+#     try:
+#         stock_data = yf.download(ticker, start='2021-8-1', end="2023-10-1") # returns a data frame
+#         stock_data.index = np.datetime_as_string(stock_data.index, unit='D') # convert date to string
+#         stock_data['Ticker'] = ticker # create a new column for ticker
+#         stock_data = stock_data.rename(columns={"Adj Close": "Adj_Close"}) # Add _ to avoid name issues in database
+#         records = stock_data.to_records(index=True) # stock data as list of tuples
+#         cur.executemany(query, records)
+#     except:
+#         pass
+
+for ticker in nasdaq_100_tickers:
     try:
-        stock_data = yf.download(ticker, start='2021-8-1', end="2023-10-1")
-        stock_data.index = np.datetime_as_string(stock_data.index, unit='D') # convert date to string
-        stock_data['Ticker'] = ticker # create a new column for ticker
+        stock_data = yf.download(ticker, start="2021-01-01", end="2024-01-01") # returns a data frame
+        stock_data.index = np.datetime_as_string(stock_data.index, unit="D") # convert date to string
+        stock_data["Ticker"] = ticker # create a new column for ticker
         stock_data = stock_data.rename(columns={"Adj Close": "Adj_Close"}) # Add _ to avoid name issues in database
         records = stock_data.to_records(index=True) # stock data as list of tuples
         cur.executemany(query, records)
@@ -89,22 +104,17 @@ for ticker in usa_tickers:
         pass
 
 
-cur.execute("SELECT COUNT(*) FROM prices;")
-count = cur.fetchone()[0]
-print(count)
-print("Query done successfully")
 
-cur.execute("SELECT COUNT(DISTINCT ticker) FROM prices;")
-count = cur.fetchone()[0]
-print(count)
-print("Query done successfully")
+# cur.execute("SELECT COUNT(*) FROM prices;")
+# count = cur.fetchone()[0]
+# print(count)
+# print("Query done successfully")
+
+# cur.execute("SELECT COUNT(DISTINCT ticker) FROM prices;")
+# count = cur.fetchone()[0]
+# print(count)
+# print("Query done successfully")
 
 
 conn.close()
-
-# TODO: try to read ticker names from a file
-# TODO: see if the dates really need to be converted to strings
-
-# TODO: UNIQUE(Date, Ticker)
-# TODO: read everything, create a subset data structure for only the stocks from USA, and then insert only that into DB
 
