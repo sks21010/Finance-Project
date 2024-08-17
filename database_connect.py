62fadc9aa9a1622230536ebc8ac10fd58fa1a5cd
sks21010 <sri_kiran.sripada@uconn.edu>
2024-08-17 12:00:39 -0400

    added foreceful db reconnection in event of failure

--- database_connect.py
+++ database_connect.py
@@ -3,18 +3,15 @@ import psycopg2
 
 @st.cache_resource(ttl=600)
 def init_connection():
-    return psycopg2.connect(**st.secrets["postgres"])
+    try: 
+        return psycopg2.connect(**st.secrets["postgres"])
+    except psycopg2.OperationalError as e:
+        st.error(f"Operational error: {e}")
+    except psycopg2.InterfaceError as e:
+        st.error(f"Interface error: {e}")
+        return psycopg2.connect(**st.secrets["postgres"])
+    except Exception as e:
+        st.error(f"Unexpected error: {e}")
 
 conn = init_connection()
-
-# @st.cache_data(ttl=600)
-# def run_query(query):
-#     with conn.cursor() as cur:
-#         cur.execute(query)
-#         return cur.fetchone()[0]
-    
-
-# sql = 'SELECT COUNT(*) FROM prices;'
-
-# st.write((run_query(sql)))
     
